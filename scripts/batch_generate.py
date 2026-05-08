5e7e650da30d02c11c524edd4de4a499220f0a61
Nausica-c <fluentedesk@gmail.com>
2026-05-08 19:00:49 +0200

    Create batch_generate.py

---
+++ scripts/batch_generate.py
@@ -0,0 +1,23 @@
+import argparse
+from scripts.generate_article import build_article
+
+def run_batch(cluster, number):
+
+    for i in range(int(number)):
+
+        title = f"{cluster.capitalize()} Guide {i+1}"
+
+        print(f"Generating: {title}")
+
+        build_article(title, cluster)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--cluster", required=True)
+    parser.add_argument("--number", required=True)
+
+    args = parser.parse_args()
+
+    run_batch(args.cluster, args.number)
